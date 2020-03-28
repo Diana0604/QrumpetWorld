b786f028204f46cca56c47a9ee202a82a520d3c8
Diana0604 <diana.vallverdu@gmail.com>
2020-03-28 09:38:25 +0000

    thrust and rotate

---
+++ Assets/Scenes/Qrumpet World/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    private void ManageVelocity()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(Vector3.forward);
+            //rigidBody.position = rigidBody.position + Vector3.forward* 0.5f;
+            //rigidBody.velocity = rigidBody.velocity + Vector3.forward*0.1f;
+        } else{
+            if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+                rigidBody.AddRelativeForce(-Vector3.forward);
+                //rigidBody.position = rigidBody.position - Vector3.forward;
+                //rigidBody.velocity = rigidBody.velocity - Vector3.forward*0.1f;
+            } else rigidBody.velocity = rigidBody.velocity * 0f;
+        }
+    }
+
+    private void ManageRotation()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down);
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ManageVelocity();
+        ManageRotation();
+        
+    }
+}
---
+++ Assets/Scenes/Rocket Game/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        rigidBody.freezeRotation = false ;
+    }
+    private void ThrustSound()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            audioData.UnPause();
+        } else {
+            audioData.Pause();
+        }
+    }
+    //check key pressed and move accordingly
+    private void ProcessInput()
+    {
+        ThrustSound();
+        Rotate();
+    }
+}
