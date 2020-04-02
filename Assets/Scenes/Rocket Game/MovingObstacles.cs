187d063fa92a4183a1721c438655e6fef512bab2
Diana0604 <diana.vallverdu@gmail.com>
2020-04-02 15:31:37 +0100

    change relative to pos

--- Assets/Scenes/Rocket Game/MovingObstacles.cs
+++ Assets/Scenes/Rocket Game/MovingObstacles.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 
 public class MovingObstacles : MonoBehaviour
 {
-    Rigidbody rigidbody;
     float dir = 1f;
-    float timeLastChange = 0f;
+    Vector3 initialPos;
     [SerializeField] float speed = 5f;
-    [SerializeField] float changeDirEach = 3.0f;
+    [SerializeField] float changeDirAfter = 3.0f;
     Vector3 userDirection = Vector3.up;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         print(Mathf.Floor(Time.time));
-
-        if(Mathf.Floor(Time.time) != timeLastChange && Mathf.Floor(Time.time)%changeDirEach == 0f)
+        Vector3 currentPos = transform.position;
+        if((currentPos - initialPos).magnitude > changeDirAfter) 
+        //if(Mathf.Floor(Time.time) != timeLastChange && Mathf.Floor(Time.time)%changeDirAfter == 0f)
         {
             print("'changing dir'");
-            timeLastChange = Mathf.Floor(Time.time);
+            //timeLastChange = Mathf.Floor(Time.time);
             dir = dir*-1f;
         }
         transform.Translate(userDirection * dir * speed * Time.deltaTime); 
