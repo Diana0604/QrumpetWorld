bdb3d230170d66bb5df8c22941358e9e5dd078d1
Diana0604 <diana.vallverdu@gmail.com>
2020-04-24 11:26:11 +0100

    prep station 1 added to the list and works fine'

---
+++ Assets/Scenes/Qrumpet World/FirstPersonCharacter/Scripts/StationsManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.FirstPerson{
+public class StationsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
